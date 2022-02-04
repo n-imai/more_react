f9ea095f7e302d072fa353ea1c88c10b949e14e8
Nobuo Imai <8196611+n-imai@users.noreply.github.com>
2022-02-04 09:35:22 +0900

    optimize app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo } from "react";
 import "./styles.css";
 import {ChildArea} from "./ChildArea";
 
@@ -9,13 +9,19 @@ export default function App() {
   const onChangeText = (e) => setText(e.target.value)
 
   const onClickOpen = () => setOpen(!open)
+
+  const onClickClose = useCallback(() => setOpen(false),[setOpen])
+
+  const temp = useMemo(() => 1+3,[]);
+  console.log(temp);
+
   return (
     <div className="App">
       <input value={text} onChange={onChangeText}/>
       <br/>
       <br/>
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose}/>
     </div>
   );
 }
